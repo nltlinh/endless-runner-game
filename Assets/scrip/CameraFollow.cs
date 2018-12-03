3abdfa58f3fbeca01dcc74f100e21efa08487acd
LinhNguyen <s3740805@rmit.edu.vn>
2018-12-03 20:59:46 +0700

    3/12 - Camera Fix

---
+++ Assets/scrip/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public araharacter thePlayer;
+
+    private Vector3 lastPlayerPosition;
+
+    private float distanceMoving;
+
+    // Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<araharacter>();
+        lastPlayerPosition = thePlayer.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceMoving = thePlayer.transform.position.x - lastPlayerPosition.x;
+
+        transform.position = new Vector3 (transform.position.x - distanceMoving, transform.position.y, transform.position.z);
+		
+        lastPlayerPosition = thePlayer.transform.position;
+	}
+}
