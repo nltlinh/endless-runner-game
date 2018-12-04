332c12cd4550641a52a92a25f78398b39932c79a
s3680599Huy <45568714+s3680599Huy@users.noreply.github.com>
2018-12-04 14:24:43 +0700

    Delete Platform

---
+++ Assets/scrip/SpawnerDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerDestroy : MonoBehaviour {
+
+    public GameObject SpawnerDestroyPoint;
+	// Use this for initialization
+	void Start () {
+        SpawnerDestroyPoint = GameObject.Find ("SpawnerDestroyPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < SpawnerDestroyPoint.transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
